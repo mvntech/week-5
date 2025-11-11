e816095b33a92ff1ede851f952cde1a3fff00cd9
mvntahax <syedamuntahapk@gmail.com>
2025-11-11 16:21:57 +0500

    5th task completed.

---
+++ task-5/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const GET_USERS = gql`
+  query getUsers {
+    users {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  
+  return (
+    <div className="user-list-container">
+      <ul>
+        {data.users.map((user) => (
+          <li key={user.id}>
+            {user.name} &rarr; {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
